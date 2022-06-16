222062e123c94413d6a0dfc77cb51dac5f90b506
Gabriel <pinheiro.gbr@gmail.com>
2022-06-15 22:18:58 -0300

    teste

--- commands/tags.py
+++ commands/tags.py
@@ -2,5 +2,5 @@ from telegram import Update
 from telegram.ext import ContextTypes, CommandHandler
 
 
-async def chegou(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def tags(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="O Pai chegou!!")
